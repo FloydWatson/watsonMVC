e6186ff97d574beda2b189bb80c6fb8d07663e6c
FloydWatson <floydbcseit@gmail.com>
2020-04-16 23:22:16 +1200

    config and autoload libraries

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL root
+    define('URLROOT', 'http://localhost/watsonmvc');
+
+    // Site Name
+    // Change this to your new name
+    define('SITENAME', 'WatsonMVC');
